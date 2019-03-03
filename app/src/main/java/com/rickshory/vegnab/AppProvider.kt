1656f5064034b41340530ff82ba3f1471ab96710
Rick Shory <rickshory@gmail.com>
2019-03-02 21:40:33 -0800

    start content provider

---
+++ app/src/main/java/com/rickshory/vegnab/AppProvider.kt
@@ -0,0 +1,43 @@
+package com.rickshory.vegnab
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * Provider for the VegNab app
+ * This is the only class that knows about [AppDatabase]
+ * */
+class AppProvider: ContentProvider() {
+    
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
