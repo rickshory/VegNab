c415d4008a5170e6ad95aa79e4f64b6b4363b377
Rick Shory <rickshory@gmail.com>
2019-03-02 21:56:12 -0800

    more content provider

--- app/src/main/java/com/rickshory/vegnab/AppProvider.kt
+++ app/src/main/java/com/rickshory/vegnab/AppProvider.kt
@@ -9,8 +9,19 @@ import android.net.Uri
  * Provider for the VegNab app
  * This is the only class that knows about [AppDatabase]
  * */
+private const val TAG = "AppProvider"
+private val CONTENT_AUTHORITY = "com.rickshory.vegnab.provider"
+private const val PROJECTS = "100"
+private const val PROJECTS_ID = "102"
+private const val VISITS = "110"
+private const val VISITS_ID = "112"
+private const val VEGITEMS = "120"
+private const val VEGITEMS_ID = "122"
+
+val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
+
 class AppProvider: ContentProvider() {
-    
+
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
