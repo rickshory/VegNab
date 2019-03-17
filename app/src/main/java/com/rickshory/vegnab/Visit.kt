85ae76a8a610f5221276664b7ab7796ac96b6b07
Rick Shory <rickshory@gmail.com>
2019-03-17 10:28:59 -0700

    id into class body

--- app/src/main/java/com/rickshory/vegnab/Visit.kt
+++ app/src/main/java/com/rickshory/vegnab/Visit.kt
@@ -4,5 +4,6 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Visit(var id: Long, val name: String, val notes: String, val location: String) : Parcelable{
+data class Visit(val name: String, val notes: String, val location: String) : Parcelable{
+    var id: Long = 0
 }
