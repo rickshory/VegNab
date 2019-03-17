990f19c411a7f1e2cc522514d2fad135587e5168
Rick Shory <rickshory@gmail.com>
2019-03-17 10:37:50 -0700

    don't use data class

--- app/src/main/java/com/rickshory/vegnab/Visit.kt
+++ app/src/main/java/com/rickshory/vegnab/Visit.kt
@@ -4,6 +4,6 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Visit(val name: String, val notes: String, val location: String) : Parcelable{
+class Visit(val name: String, val notes: String, val location: String) : Parcelable{
     var id: Long = 0
 }
