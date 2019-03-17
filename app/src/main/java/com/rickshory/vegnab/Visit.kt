05c02b716faf04d95cb5c765d2a1dace78b386ed
Rick Shory <rickshory@gmail.com>
2019-03-17 10:20:56 -0700

    add Parcelize imort

--- app/src/main/java/com/rickshory/vegnab/Visit.kt
+++ app/src/main/java/com/rickshory/vegnab/Visit.kt
@@ -1,6 +1,7 @@
 package com.rickshory.vegnab
 
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Visit(var id: Long, val name: String, val notes: String, val location: String) : Parcelable{
