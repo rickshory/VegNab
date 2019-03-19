b6f7b35aec47f8e18f763d0aede7c5294e9a4db4
Rick Shory <rickshory@gmail.com>
2019-03-18 19:36:37 -0700

    work on VAE fragment

--- app/src/main/java/com/rickshory/vegnab/FragmentVisitAddEdit.kt
+++ app/src/main/java/com/rickshory/vegnab/FragmentVisitAddEdit.kt
@@ -11,8 +11,7 @@ import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_VISIT = "visit"
 
 /**
  * A simple [Fragment] subclass.
