c9cbc6e57de3f885b90002d0522c80f1b1546dd8
Rick Shory <rickshory@gmail.com>
2019-03-24 09:53:52 -0700

    try 'Tags' file
    copied from old version of VegNab, will change

---
+++ app/src/main/java/com/rickshory/vegnab/Tags.kt
@@ -0,0 +1,20 @@
+package com.rickshory.vegnab
+
+object Tags {
+    const val SPINNER_FIRST_USE = "FirstTime"; // flag to catch and ignore erroneous first firing
+    const val NEW_VISIT = "NewVisit";
+    const val VISIT_HEADER = "VisitHeader";
+    const val TEST_WEBVIEW = "TestWebview";
+    const val WEBVIEW_TUTORIAL = "WebviewTutorial";
+    const val WEBVIEW_PLOT_TYPES = "WebviewPlotTypes";
+    const val WEBVIEW_REGIONAL_LISTS = "WebviewSppLists";
+    const val DATA_SCREENS_CONTAINER = "DataScreensContainer";
+    const val VEG_SUBPLOT = "VegSubplot";
+    const val SELECT_SPECIES = "SelectSpecies";
+    const val MANAGE_PLACEHOLDERS = "ManagePlaceholders";
+    const val MANAGE_SPELLINGS = "ManageSpellings";
+    const val CLOUD_STORAGE = "CloudStorage";
+    const val EDIT_PLACEHOLDER = "EditPlaceholder";
+    const val PLACEHOLDER_PIX_GRID = "PlaceholderPixGrid";
+    const val PLACEHOLDER_PIC_DETAIL = "PlaceholderPicDetail";
+}
