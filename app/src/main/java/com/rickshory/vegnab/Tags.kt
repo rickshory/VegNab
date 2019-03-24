4884f8be46e8d491ec34012912b6a1d013427ea4
Rick Shory <rickshory@gmail.com>
2019-03-24 10:00:31 -0700

    remove unneeded ';'s

--- app/src/main/java/com/rickshory/vegnab/Tags.kt
+++ app/src/main/java/com/rickshory/vegnab/Tags.kt
@@ -1,22 +1,22 @@
 package com.rickshory.vegnab
 
 object Tags {
-    const val SPINNER_FIRST_USE = "FirstTime"; // flag to catch and ignore erroneous first firing
-    const val NEW_VISIT = "NewVisit";
+    const val SPINNER_FIRST_USE = "FirstTime" // flag to catch and ignore erroneous first firing
+    const val NEW_VISIT = "NewVisit"
 
-    const val TEST_WEBVIEW = "TestWebview";
-    const val WEBVIEW_TUTORIAL = "WebviewTutorial";
-    const val WEBVIEW_PLOT_TYPES = "WebviewPlotTypes";
-    const val WEBVIEW_REGIONAL_LISTS = "WebviewSppLists";
-    const val DATA_SCREENS_CONTAINER = "DataScreensContainer";
-    const val VEG_SUBPLOT = "VegSubplot";
-    const val SELECT_SPECIES = "SelectSpecies";
-    const val MANAGE_PLACEHOLDERS = "ManagePlaceholders";
-    const val MANAGE_SPELLINGS = "ManageSpellings";
-    const val CLOUD_STORAGE = "CloudStorage";
-    const val EDIT_PLACEHOLDER = "EditPlaceholder";
-    const val PLACEHOLDER_PIX_GRID = "PlaceholderPixGrid";
-    const val PLACEHOLDER_PIC_DETAIL = "PlaceholderPicDetail";
+    const val TEST_WEBVIEW = "TestWebview"
+    const val WEBVIEW_TUTORIAL = "WebviewTutorial"
+    const val WEBVIEW_PLOT_TYPES = "WebviewPlotTypes"
+    const val WEBVIEW_REGIONAL_LISTS = "WebviewSppLists"
+    const val DATA_SCREENS_CONTAINER = "DataScreensContainer"
+    const val VEG_SUBPLOT = "VegSubplot"
+    const val SELECT_SPECIES = "SelectSpecies"
+    const val MANAGE_PLACEHOLDERS = "ManagePlaceholders"
+    const val MANAGE_SPELLINGS = "ManageSpellings"
+    const val CLOUD_STORAGE = "CloudStorage"
+    const val EDIT_PLACEHOLDER = "EditPlaceholder"
+    const val PLACEHOLDER_PIX_GRID = "PlaceholderPixGrid"
+    const val PLACEHOLDER_PIC_DETAIL = "PlaceholderPicDetail"
     object Fragments { // track which fragment is current
         const val VISITS_LIST = "VisitsList"
         const val VISIT_HEADER = "VisitHeader"
