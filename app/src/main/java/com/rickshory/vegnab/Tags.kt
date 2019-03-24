e03a9bdc67ab0132be6846d0c4ea76d1265482a3
Rick Shory <rickshory@gmail.com>
2019-03-24 09:59:07 -0700

    try Tags.Fragments structure

--- app/src/main/java/com/rickshory/vegnab/Tags.kt
+++ app/src/main/java/com/rickshory/vegnab/Tags.kt
@@ -3,7 +3,7 @@ package com.rickshory.vegnab
 object Tags {
     const val SPINNER_FIRST_USE = "FirstTime"; // flag to catch and ignore erroneous first firing
     const val NEW_VISIT = "NewVisit";
-    const val VISIT_HEADER = "VisitHeader";
+
     const val TEST_WEBVIEW = "TestWebview";
     const val WEBVIEW_TUTORIAL = "WebviewTutorial";
     const val WEBVIEW_PLOT_TYPES = "WebviewPlotTypes";
@@ -17,4 +17,9 @@ object Tags {
     const val EDIT_PLACEHOLDER = "EditPlaceholder";
     const val PLACEHOLDER_PIX_GRID = "PlaceholderPixGrid";
     const val PLACEHOLDER_PIC_DETAIL = "PlaceholderPicDetail";
+    object Fragments { // track which fragment is current
+        const val VISITS_LIST = "VisitsList"
+        const val VISIT_HEADER = "VisitHeader"
+
+    }
 }
