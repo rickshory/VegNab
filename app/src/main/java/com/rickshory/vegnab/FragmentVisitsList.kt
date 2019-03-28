880a07ee9af24693e5f3340b22a5bfe5916168c6
Rick Shory <rickshory@gmail.com>
2019-03-27 17:19:16 -0700

    finish VisitsList

--- app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
@@ -1,5 +1,6 @@
 package com.rickshory.vegnab
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.util.Log
@@ -42,4 +43,62 @@ class FragmentVisitsList : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_visits, container, false)
     }
+
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    fun onButtonPressed(uri: Uri) {
+//        listener?.onGoClicked(uri)
+//    }
+
+    override fun onAttach(context: Context) {
+        Log.d(TAG, "onAttach: starts")
+        super.onAttach(context)
+        if (context is VisitsListInterface) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement VisitsListInterface")
+        }
+    }
+
+    override fun onDetach() {
+        Log.d(TAG, "onDetach: starts")
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface VisitsListInterface {
+        // TODO: Update argument type and name
+//        fun onGoClicked(uri: Uri)
+        fun onGoClicked()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param visit The visit to edit, or null to add a new visit
+         * @return A new instance of fragment FragmentVisitAddEdit.
+         */
+        @JvmStatic
+        fun newInstance(visitsListOpts: VisitsListOpts?) =
+            FragmentVisitsList().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_VIS_LIST_OPTS, visitsListOpts)
+//                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
 }
