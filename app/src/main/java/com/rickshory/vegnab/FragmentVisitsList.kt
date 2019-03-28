05dab2486dcbe5204c58888e75b09af0c893a25f
Rick Shory <rickshory@gmail.com>
2019-03-27 17:13:39 -0700

    more VisitsList

--- app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
@@ -2,6 +2,7 @@ package com.rickshory.vegnab
 
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,11 +20,26 @@ private const val TAG = "FragmentVisitsList"
 private const val ARG_VIS_LIST_OPTS = "visListOpts"
 
 class FragmentVisitsList : Fragment() {
-//VisitsListOpts
+    private var visitsListOpts: VisitsListOpts? = null
+    //    private var param2: String? = null
+    private var listener: VisitsListInterface? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: starts")
+        super.onCreate(savedInstanceState)
+        visitsListOpts = arguments?.getParcelable(ARG_VIS_LIST_OPTS)
+//        arguments?.let {
+//            visit = it.getString(ARG_VISIT)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(TAG, "onCreateView: starts")
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_visits, container, false)
     }
 }
