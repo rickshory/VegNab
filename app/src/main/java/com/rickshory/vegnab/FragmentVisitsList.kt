2f1558dea2c4ad59dae5573042587249675db250
Rick Shory <rickshory@gmail.com>
2019-03-30 16:31:37 -0700

    try context; still wrong

--- app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/FragmentVisitsList.kt
@@ -68,7 +68,7 @@ class FragmentVisitsList : Fragment() {
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
             Log.d(TAG, "fab_new_visit action: start")
-            visitsListOnGoClicked()
+            context.visitsListOnGoClicked()
             Log.d(TAG, "fab_new_visit action: exit")
         }
     }
