3983b97503032a22a406dd7e8a161721d7bab5d7
Rick Shory <rickshory@gmail.com>
2019-04-13 13:19:03 -0700

    finish onCreateViewHolder

--- app/src/main/java/com/rickshory/vegnab/CursorRecyclerViewAdapter.kt
+++ app/src/main/java/com/rickshory/vegnab/CursorRecyclerViewAdapter.kt
@@ -2,6 +2,7 @@ package com.rickshory.vegnab
 
 import android.database.Cursor
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -22,13 +23,9 @@ class CursorRecyclerViewAdapter(private var cursor: Cursor?):
     /**
      * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
      * an item.
-     *
-     *
      * This new ViewHolder should be constructed with a new View that can represent the items
      * of the given type. You can either create a new View manually or inflate it from an XML
      * layout file.
-     *
-     *
      * The new ViewHolder will be used to display items of the adapter using
      * [.onBindViewHolder]. Since it will be re-used to display
      * different items in the data set, it is a good idea to cache references to sub views of
@@ -44,14 +41,14 @@ class CursorRecyclerViewAdapter(private var cursor: Cursor?):
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VisitViewHolder {
         Log.d(TAG,"onCreateViewHolder: new View requested")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.visit_list_item, parent, false)
+        return VisitViewHolder(view)
     }
 
     /**
      * Called by RecyclerView to display the data at the specified position. This method should
      * update the contents of the [ViewHolder.itemView] to reflect the item at the given
      * position.
-     *
-     *
      * Note that unlike [android.widget.ListView], RecyclerView will not call this method
      * again if the position of the item changes in the data set unless the item itself is
      * invalidated or the new position cannot be determined. For this reason, you should only
@@ -59,9 +56,7 @@ class CursorRecyclerViewAdapter(private var cursor: Cursor?):
      * this method and should not keep a copy of it. If you need the position of an item later
      * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
      * have the updated adapter position.
-     *
-     * Override [.onBindViewHolder] instead if Adapter can
-     * handle efficient partial bind.
+     * Override [.onBindViewHolder] instead if Adapter can handle efficient partial bind.
      *
      * @param holder The ViewHolder which should be updated to represent the contents of the
      * item at the given position in the data set.
