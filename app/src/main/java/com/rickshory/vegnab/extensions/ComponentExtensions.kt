cf33e04cacbc11f21ec122668d2bfd7bad50c50b
Rick Shory <rickshory@gmail.com>
2019-04-16 21:48:25 -0700

    afterTextChanged fn

--- app/src/main/java/com/rickshory/vegnab/extensions/ComponentExtensions.kt
+++ app/src/main/java/com/rickshory/vegnab/extensions/ComponentExtensions.kt
@@ -1,10 +1,22 @@
 package com.rickshory.vegnab.extensions
 
 import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 fun Editable.set(content: String) {
     clear()
     append(content)
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+    })
 }
 
 class ComponentExtensions {
