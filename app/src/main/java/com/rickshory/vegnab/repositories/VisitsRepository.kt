6e332fa443dd6ef151525e6f275b582bd0ee9c22
Rick Shory <rickshory@gmail.com>
2019-04-17 21:12:31 -0700

    VisitsRepository interface

---
+++ app/src/main/java/com/rickshory/vegnab/repositories/VisitsRepository.kt
@@ -0,0 +1,8 @@
+package com.rickshory.vegnab.repositories
+
+import com.rickshory.vegnab.models.Visit
+
+interface VisitsRepository {
+    fun getVisitById(visitId : Long) : Visit
+    fun saveVisit(item: Visit)
+}
