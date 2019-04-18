c18142dacc135451634d97a15c10b047a600b427
Rick Shory <rickshory@gmail.com>
2019-04-17 21:19:22 -0700

    add companion object

--- app/src/main/java/com/rickshory/vegnab/repositories/MockVisitsRepository.kt
+++ app/src/main/java/com/rickshory/vegnab/repositories/MockVisitsRepository.kt
@@ -3,6 +3,12 @@ package com.rickshory.vegnab.repositories
 import com.rickshory.vegnab.models.Visit
 
 class MockVisitsRepository : VisitsRepository {
+
+    companion object {
+        private val TAG = this::class.java.simpleName
+        val instance = MockVisitsRepository()
+    }
+    
     override fun getVisitById(visitId: Long): Visit {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
