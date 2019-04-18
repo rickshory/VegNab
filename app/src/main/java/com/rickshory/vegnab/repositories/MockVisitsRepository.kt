44d3c120fdf2ea002b9554037dd5d9b3d812b47e
Rick Shory <rickshory@gmail.com>
2019-04-17 21:15:49 -0700

    start MockVisitsRepository

---
+++ app/src/main/java/com/rickshory/vegnab/repositories/MockVisitsRepository.kt
@@ -0,0 +1,13 @@
+package com.rickshory.vegnab.repositories
+
+import com.rickshory.vegnab.models.Visit
+
+class MockVisitsRepository : VisitsRepository {
+    override fun getVisitById(visitId: Long): Visit {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saveVisit(item: Visit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
