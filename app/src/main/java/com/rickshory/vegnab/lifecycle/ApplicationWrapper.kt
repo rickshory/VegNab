6b8586cc513e378bb6b011d34e30db13c3d78f0e
Rick Shory <rickshory@gmail.com>
2019-04-19 07:05:55 -0700

    add bean

--- app/src/main/java/com/rickshory/vegnab/lifecycle/ApplicationWrapper.kt
+++ app/src/main/java/com/rickshory/vegnab/lifecycle/ApplicationWrapper.kt
@@ -1,12 +1,17 @@
 package com.rickshory.vegnab.lifecycle
 
 import android.app.Application
+import com.rickshory.vegnab.repositories.MockVisitsRepository
+import com.rickshory.vegnab.repositories.VisitsRepository
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.applicationContext
 
 class ApplicationWrapper : Application() {
     companion object {
-        private val modules : Module = applicationContext {  }
+        private val modules : Module = applicationContext {
+            bean { MockVisitsRepository() as VisitsRepository }
+            
+        }
     }
     override fun onCreate() {
         super.onCreate()
