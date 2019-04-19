6ccd8b0555e9d4b930ad48d3d3d324b834e7f00b
Rick Shory <rickshory@gmail.com>
2019-04-19 07:02:00 -0700

    start use koin

--- app/src/main/java/com/rickshory/vegnab/lifecycle/ApplicationWrapper.kt
+++ app/src/main/java/com/rickshory/vegnab/lifecycle/ApplicationWrapper.kt
@@ -1,8 +1,13 @@
 package com.rickshory.vegnab.lifecycle
 
 import android.app.Application
+import org.koin.dsl.module.Module
+import org.koin.dsl.module.applicationContext
 
 class ApplicationWrapper : Application() {
+    companion object {
+        private val modules : Module = applicationContext {  }
+    }
     override fun onCreate() {
         super.onCreate()
     }
