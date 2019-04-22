b6109a77c4535148c4ed89fda8e5b2a0c43e2150
Rick Shory <rickshory@gmail.com>
2019-04-22 14:31:14 -0700

    try first entity 'Visits'

--- app/src/main/java/com/rickshory/vegnab/roomdb/entities/Visit.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/entities/Visit.kt
@@ -1,4 +1,12 @@
 package com.rickshory.vegnab.roomdb.entities
 
-data class Visit(val id: Long, val name: String){
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Visits")
+class Visit(@PrimaryKey @ColumnInfo(name = "id") val id: Long,
+            @ColumnInfo(name = "name") val name: String,
+            @ColumnInfo(name = "notes") val notes: String)
+{
 }
