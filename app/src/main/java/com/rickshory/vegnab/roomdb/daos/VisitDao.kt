80ba45621b8e35268277921ab8d0e3ac5fa75844
Rick Shory <rickshory@gmail.com>
2019-04-24 11:17:33 -0700

    OnConflictStrategy

--- app/src/main/java/com/rickshory/vegnab/roomdb/daos/VisitDao.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/daos/VisitDao.kt
@@ -2,6 +2,7 @@ package com.rickshory.vegnab.roomdb.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.rickshory.vegnab.roomdb.entities.Visit
 
@@ -11,7 +12,7 @@ interface VisitDao {
     @Query("SELECT * from Visits")
     fun getAllVisits(): List<Visit>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(visit: Visit)
 
     @Query("DELETE FROM Visits")
