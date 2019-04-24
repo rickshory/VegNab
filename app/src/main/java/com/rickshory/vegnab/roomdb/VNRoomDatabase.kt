41dd9aa0b83ff23dbf1eae8d17779e35f6a96d25
Rick Shory <rickshory@gmail.com>
2019-04-24 11:36:46 -0700

    annotate Database

--- app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
@@ -1,7 +1,11 @@
 package com.rickshory.vegnab.roomdb
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.rickshory.vegnab.roomdb.entities.Visit
 
+@Database(entities = [Visit::class], version = 1)
 public abstract class VNRoomDatabase: RoomDatabase() {
+
 
 }
