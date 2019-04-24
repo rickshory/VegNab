4ce485313adcb0914da71ababd4851d5a719ec2b
Rick Shory <rickshory@gmail.com>
2019-04-24 11:39:23 -0700

    abstract getter

--- app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
@@ -2,10 +2,11 @@ package com.rickshory.vegnab.roomdb
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.rickshory.vegnab.roomdb.daos.VisitDao
 import com.rickshory.vegnab.roomdb.entities.Visit
 
 @Database(entities = [Visit::class], version = 1)
 public abstract class VNRoomDatabase: RoomDatabase() {
-
+    abstract fun VisitDao(): VisitDao
 
 }
