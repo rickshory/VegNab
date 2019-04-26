edde1bd0598df14926f60dce096bf2c52d1f64e2
Rick Shory <rickshory@gmail.com>
2019-04-25 23:06:29 -0700

    instance

--- app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.rickshory.vegnab.roomdb.daos.VisitDao
 import com.rickshory.vegnab.roomdb.entities.Visit
+import org.koin.dsl.context.Context
 
 @Database(entities = [Visit::class], version = 1)
 public abstract class VNRoomDatabase: RoomDatabase() {
@@ -14,4 +15,13 @@ public abstract class VNRoomDatabase: RoomDatabase() {
 companion object {
     @Volatile
     private var INSTANCE: VNRoomDatabase? = null
+
+    fun getDatabase(context: Context) : VNRoomDatabase {
+        return INSTANCE ?: synchronized(this) { // if existing, lock
+            // else create instance
+            val instance = // TODO
+            INSTANCE = instance
+            instance
+        }
+    }
 }
