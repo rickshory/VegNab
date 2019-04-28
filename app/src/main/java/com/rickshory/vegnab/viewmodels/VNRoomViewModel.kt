fde7c0e425f9fb81f374f9a9451e6b985fbab2f4
Rick Shory <rickshory@gmail.com>
2019-04-28 12:30:47 -0700

    private member to hold ref to repository

--- app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
+++ app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
@@ -2,6 +2,8 @@ package com.rickshory.vegnab.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.rickshory.vegnab.repositories.VNRoomRepository
 
 class VNRoomViewModel (app: Application) : AndroidViewModel(app)  {
+    private val repos: VNRoomRepository
 }
