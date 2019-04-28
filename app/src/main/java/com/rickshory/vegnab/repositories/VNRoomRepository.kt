805f33a77a9a098bdc414f74e2c3b1dc4b51bc1e
Rick Shory <rickshory@gmail.com>
2019-04-28 11:53:44 -0700

    visitDao private property

--- app/src/main/java/com/rickshory/vegnab/repositories/VNRoomRepository.kt
+++ app/src/main/java/com/rickshory/vegnab/repositories/VNRoomRepository.kt
@@ -1,4 +1,6 @@
 package com.rickshory.vegnab.repositories
 
-class VNRoomRepository {
+import com.rickshory.vegnab.roomdb.daos.VisitDao
+
+class VNRoomRepository (private val visitDao: VisitDao) {
 }
