a7fc798b0fd36ba36a55fdefffae0ce52f10b99f
Rick Shory <rickshory@gmail.com>
2019-04-28 12:34:45 -0700

    private LiveData member to cache Visits list

--- app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
+++ app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
@@ -2,8 +2,11 @@ package com.rickshory.vegnab.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import com.rickshory.vegnab.repositories.VNRoomRepository
+import com.rickshory.vegnab.roomdb.entities.Visit
 
 class VNRoomViewModel (app: Application) : AndroidViewModel(app)  {
     private val repos: VNRoomRepository
+    val allVisits: LiveData<List<Visit>>
 }
