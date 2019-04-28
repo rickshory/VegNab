dac751b5a4db0ffd12072eae853cb62de254d937
Rick Shory <rickshory@gmail.com>
2019-04-28 12:24:35 -0700

    extend AndroidViewModel

--- app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
+++ app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
@@ -1,6 +1,7 @@
 package com.rickshory.vegnab.viewmodels
 
 import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 
-class VNRoomViewModel (app: Application) {
+class VNRoomViewModel (app: Application) : AndroidViewModel(app)  {
 }
