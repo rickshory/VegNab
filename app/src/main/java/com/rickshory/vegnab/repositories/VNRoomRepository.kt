74c3b7910bf00573b562e36eb8f7aadee1b2215d
Rick Shory <rickshory@gmail.com>
2019-04-28 12:00:46 -0700

    Visits list as public property

--- app/src/main/java/com/rickshory/vegnab/repositories/VNRoomRepository.kt
+++ app/src/main/java/com/rickshory/vegnab/repositories/VNRoomRepository.kt
@@ -1,6 +1,9 @@
 package com.rickshory.vegnab.repositories
 
+import androidx.lifecycle.LiveData
 import com.rickshory.vegnab.roomdb.daos.VisitDao
+import com.rickshory.vegnab.roomdb.entities.Visit
 
 class VNRoomRepository (private val visitDao: VisitDao) {
+    val allVisits: LiveData<List<Visit>>
 }
