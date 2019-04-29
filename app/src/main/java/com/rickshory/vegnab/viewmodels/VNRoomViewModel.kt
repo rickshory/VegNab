019109fd33b186596232bbdea338eed056bb9e25
Rick Shory <rickshory@gmail.com>
2019-04-29 11:27:35 -0700

    start init block

--- app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
+++ app/src/main/java/com/rickshory/vegnab/viewmodels/VNRoomViewModel.kt
@@ -7,6 +7,10 @@ import com.rickshory.vegnab.repositories.VNRoomRepository
 import com.rickshory.vegnab.roomdb.entities.Visit
 
 class VNRoomViewModel (app: Application) : AndroidViewModel(app)  {
-    private val repos: VNRoomRepository
+    private val repo: VNRoomRepository
     val allVisits: LiveData<List<Visit>>
+
+    init {
+        
+    }
 }
