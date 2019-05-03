d38156b7cca5b80c791e62970d2f2fbef491d2d0
Rick Shory <rickshory@gmail.com>
2019-05-02 20:59:19 -0700

    inner class VisitsViewHolder

--- app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
+++ app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
@@ -1,9 +1,17 @@
 package com.rickshory.vegnab.adapters
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.visit_list_item.view.*
 
 class VisitsListAdapter internal constructor(
     context: Context
 ) : RecyclerView.Adapter<VisitsListAdapter.VisitsViewHolder()> {
+
+
+    inner class VisitsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameItemView: TextView = itemView.vli_name
+    }
 }
