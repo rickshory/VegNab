744b80ae9ff9c49846cd9c130892e4c32759a3ba
Rick Shory <rickshory@gmail.com>
2019-05-02 21:06:33 -0700

    stub overrides

--- app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
+++ app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
@@ -2,6 +2,7 @@ package com.rickshory.vegnab.adapters
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.visit_list_item.view.*
@@ -14,6 +15,16 @@ class VisitsListAdapter internal constructor(
     inner class VisitsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameItemView: TextView = itemView.vli_name
     }
-    
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VisitsViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: VisitsViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
