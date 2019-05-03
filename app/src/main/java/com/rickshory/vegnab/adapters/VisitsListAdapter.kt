21b0235a3f425f635b0fa077de6be702cfc7a856
Rick Shory <rickshory@gmail.com>
2019-05-02 20:45:57 -0700

    extends

--- app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
+++ app/src/main/java/com/rickshory/vegnab/adapters/VisitsListAdapter.kt
@@ -1,8 +1,9 @@
 package com.rickshory.vegnab.adapters
 
 import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
 
 class VisitsListAdapter internal constructor(
     context: Context
-) {
+) : RecyclerView.Adapter<VisitsListAdapter.VisitsViewHolder()> {
 }
