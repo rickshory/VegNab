3e340bd0db949f0443cb9901f3e7003c6eee0d45
Rick Shory <rickshory@gmail.com>
2019-05-08 21:55:07 -0700

    CoroutineScope

--- app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
+++ app/src/main/java/com/rickshory/vegnab/roomdb/VNRoomDatabase.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.rickshory.vegnab.roomdb.daos.VisitDao
 import com.rickshory.vegnab.roomdb.entities.Visit
+import kotlinx.coroutines.CoroutineScope
+
 //import org.koin.dsl.context.Context
 
 @Database(entities = arrayOf(Visit::class), version = 1)
@@ -17,7 +19,9 @@ public abstract class VNRoomDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: VNRoomDatabase? = null
 
-        fun getDatabase(context: Context): VNRoomDatabase {
+        fun getDatabase(context: Context,
+                        scope: CoroutineScope
+        ): VNRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
