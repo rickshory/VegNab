a02391acaaaba56dc38dc74f1c688a841e8a18ce
Rick Shory <rickshory@gmail.com>
2019-05-15 21:36:00 -0700

    add NewVisitActivity

---
+++ app/src/main/java/com/rickshory/vegnab/NewVisitActivity.kt
@@ -0,0 +1,12 @@
+package com.rickshory.vegnab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewVisitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_visit)
+    }
+}
