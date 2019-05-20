5603a7b0ddf2190d2c72f87dba2c24b9e0d3a4b6
Rick Shory <rickshory@gmail.com>
2019-05-19 21:56:38 -0700

    editVisitNameView

--- app/src/main/java/com/rickshory/vegnab/NewVisitActivity.kt
+++ app/src/main/java/com/rickshory/vegnab/NewVisitActivity.kt
@@ -3,6 +3,7 @@ package com.rickshory.vegnab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_new_visit.*
 
 class NewVisitActivity : AppCompatActivity() {
     private lateinit var editVisitNameView: TextView
@@ -11,5 +12,6 @@ class NewVisitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_visit)
+        editVisitNameView = edit_visit_name
     }
 }
