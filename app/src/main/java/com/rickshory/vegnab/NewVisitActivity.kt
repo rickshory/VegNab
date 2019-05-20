56bc291b598a0549cc9a19aea0771cc4fd3cc428
Rick Shory <rickshory@gmail.com>
2019-05-19 22:01:39 -0700

    btnSaveVisit

--- app/src/main/java/com/rickshory/vegnab/NewVisitActivity.kt
+++ app/src/main/java/com/rickshory/vegnab/NewVisitActivity.kt
@@ -2,6 +2,7 @@ package com.rickshory.vegnab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_new_visit.*
 
@@ -14,5 +15,6 @@ class NewVisitActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_visit)
         editVisitNameView = edit_visit_name
         editVisitDescriptionView = edit_visit_description
+        val btnSaveVisit = findViewById<Button>(R.id.btn_save_visit)
     }
 }
