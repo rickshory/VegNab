3254fecfd7ebfade5eb9bd69df092bd3bb6d999a
Rick Shory <rickshory@gmail.com>
2019-05-21 22:11:29 -0700

    setVisits; bug

--- app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
@@ -50,9 +50,9 @@ class FragmentVisitsList : Fragment() {
 //            visit = it.getString(ARG_VISIT)
 //            param2 = it.getString(ARG_PARAM2)
 //        }
-        
+
         vnRoomViewModel = activity?.run {
-            vlAdapter = VisitsListAdapter(this.context)
+            vlAdapter = VisitsListAdapter(this)
             ViewModelProviders.of(this).get(VNRoomViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
         vnRoomViewModel.allVis.observe(this, Observer {visits_list ->
