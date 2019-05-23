bfe889b27b69364f6e3994afc028431cafc14024
Rick Shory <rickshory@gmail.com>
2019-05-22 18:47:27 -0700

    try define vlAdapter outside run scope

--- app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
@@ -50,10 +50,12 @@ class FragmentVisitsList : Fragment() {
 //            visit = it.getString(ARG_VISIT)
 //            param2 = it.getString(ARG_PARAM2)
 //        }
+        vlAdapter = VisitsListAdapter(activity.applicationContext)
         vnRoomViewModel = activity?.run {
-            vlAdapter = VisitsListAdapter(this)
+
             ViewModelProviders.of(this).get(VNRoomViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
+
         vnRoomViewModel.allVis.observe(this, Observer {visits_list ->
             // update the cached copy of visits in the adapter
             visits_list?.let { vlAdapter.setVisits{it}}
