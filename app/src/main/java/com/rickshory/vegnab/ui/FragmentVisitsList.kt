ce2037d46962471a0b3a4bd0f464779fb64321bd
Rick Shory <rickshory@gmail.com>
2019-05-27 21:31:48 -0700

    try diagnostics

--- app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
@@ -58,7 +58,8 @@ class FragmentVisitsList : Fragment() {
 
         vnRoomViewModel.allVis.observe(this, Observer {visits_list ->
             // update the cached copy of visits in the adapter
-            visits_list?.let{{VisitsListAdapter().setVisits(it)}.also{VisitsListAdapter().itemCount} }
+            visits_list?.let{{VisitsListAdapter().setVisits(it)}
+                .also{Log.d(TAG, "Visit items in DB ${VisitsListAdapter().itemCount}")} }
         })
     }
 
