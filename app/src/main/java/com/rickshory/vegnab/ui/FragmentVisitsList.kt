9fd90f6e54963a3827f99bb394a51ff1b65a06d1
Rick Shory <rickshory@gmail.com>
2019-05-29 16:42:50 -0700

    put back

--- app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
+++ app/src/main/java/com/rickshory/vegnab/ui/FragmentVisitsList.kt
@@ -33,13 +33,13 @@ import com.rickshory.vegnab.ui.vlAdapter as vlAdapter
 private const val ARG_VIS_LIST_OPTS = "visListOpts"
 
 private lateinit var vnRoomViewModel: VNRoomViewModel
-
+private lateinit var vlAdapter: RecyclerView.Adapter<*>
 private lateinit var vlRecycView: RecyclerView
 
 private lateinit var viewManager: RecyclerView.LayoutManager
 
 class FragmentVisitsList : Fragment() {
-    private lateinit var vlAdapter: RecyclerView.Adapter<*>
+
     private val TAG = this::class.java.simpleName
     private var visitsListOpts: VisitsListOpts? = null
     //    private var param2: String? = null
